3ed025869a613452e72962efd58e5aaefb5c0eaa
b <brightmart@hotmail.com>
2019-10-04 01:24:35 +0800

    update

--- run_classifier.py
+++ run_classifier.py
@@ -201,52 +201,6 @@ class DataProcessor(object):
         lines.append(line)
       return lines
 
-
-class LCQMCPairClassificationProcessor(DataProcessor): # TODO NEED CHANGE2
-  """Processor for the internal data set. sentence pair classification"""
-  def __init__(self):
-    self.language = "zh"
-
-  def get_train_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "train.txt")), "train")
-    # dev_0827.tsv
-
-  def get_dev_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev.txt")), "dev") # dev
-
-  def get_test_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "test.txt")), "test")
-
-  def get_labels(self):
-    """See base class."""
-    return ["0", "1"]
-
-  def _create_examples(self, lines, set_type):
-    """Creates examples for the training and dev sets."""
-    examples = []
-    print("length of lines:",len(lines))
-    for (i, line) in enumerate(lines):
-      #print('#i:',i,line)
-      if i == 0:
-        continue
-      guid = "%s-%s" % (set_type, i)
-      try:
-          label = tokenization.convert_to_unicode(line[2])
-          text_a = tokenization.convert_to_unicode(line[0])
-          text_b = tokenization.convert_to_unicode(line[1])
-          examples.append(
-              InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
-      except Exception:
-          print('###error.i:', i, line)
-    return examples
-
-
 def convert_single_example(ex_index, example, label_list, max_seq_length,
                            tokenizer):
   """Converts a single `InputExample` into a single `InputFeatures`."""
@@ -656,7 +610,7 @@ class LCQMCPairClassificationProcessor(DataProcessor): # TODO NEED CHANGE2
   def get_dev_examples(self, data_dir):
     """See base class."""
     return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, "dev.txt")), "dev") # todo change temp for test purpose
+        self._read_tsv(os.path.join(data_dir, "dev.txt")), "dev")
 
   def get_test_examples(self, data_dir):
     """See base class."""
